9ecb3965d450b1f582873b4f91f2be2eec14a0e2
trusktr <trusktr@gmail.com>
2013-07-16 05:27:43 -0700

    Added a custom .getElementsByClass() method for browsers that dont support it.

--- dead.js
+++ dead.js
@@ -1,7 +1,26 @@
 /*
  *A javascript framework simple enough even for zombies.
  */
-function x_x ( victim ) { // to target elements in the page by id, class, or tag name
+function x_x ( victim ) { // Use this function to target html elements (victims) by id, class, or tag name.
+
+        /*create the getelementsbyclassname() method for the document
+          for use with x_x() if it doesn't already exist (older browsers).*/
+        if (document.getElementsByClassName == undefined) {
+                document.getElementsByClassName = function(className) {
+                        var hasClassName = new RegExp("(?:^|\\s)" + className + "(?:$|\\s)");
+                        var allElements = document.getElementsByTagName("*");
+                        var results = [];
+
+                        var element;
+                        for (var i = 0; (element = allElements[i]) != null; i++) {
+                                var elementClass = element.className;
+                                if (elementClass && elementClass.indexOf(className) != -1 && hasClassName.test(elementClass))
+                                        results.push(element);
+                        }
+
+                        return results;
+                }
+        }
 
         if ( victim.substr(0, 1) == '#' ) {
                 return document.getElementById( victim.substr(1) );
@@ -12,4 +31,5 @@ function x_x ( victim ) { // to target elements in the page by id, class, or tag
         else if ( victim.substr(0, 1).match('[a-z]+') || victim.substr(0) == '*' ) {
                 return document.getElementsByTagName( victim.substr(1) );
         }
+
 }
